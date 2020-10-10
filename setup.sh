850048f3b512a22965d6bc54956fe7a9c7e37eb7
John Hannagan <weetbix_@hotmail.com>
2020-10-10 19:25:26 +0200

    Add setup & vim colours

---
+++ setup.sh
@@ -0,0 +1,5 @@
+SCRIPT_DIR=$(cd -P "$(dirname "$BASH_SOURCE")"; pwd)
+ln -fs $SCRIPT_DIR/.zshrc ~/.zshrc
+ln -fs $SCRIPT_DIR/.vimrc ~/.vimrc
+mkdir -p ~/.vim
+ln -fs $SCRIPT_DIR/vim-colors ~/.vim/colors
