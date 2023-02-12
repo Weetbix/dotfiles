0e62af3474266acfdcb761dd4854a06dab909e59
John Hannagan <weetbix_@hotmail.com>
2023-02-12 20:45:32 +1100

    Add hdutils key remapping

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 SCRIPT_DIR=$(cd -P "$(dirname "$BASH_SOURCE")"; pwd)
+ln -fs $SCRIPT_DIR/com.local.KeyRemapping.plist ~/Library/LaunchAgents/com.local.KeyRemapping.plist
 ln -fs $SCRIPT_DIR/.zshrc ~/.zshrc
 ln -fs $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
 ln -fs $SCRIPT_DIR/.vimrc ~/.vimrc
