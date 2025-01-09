001ffc3315e751eb62f2339708c0178e29430005
John Hannagan <weetbix_@hotmail.com>
2025-01-09 14:35:07 +1100

    Add git config

--- setup.sh
+++ setup.sh
@@ -7,9 +7,10 @@ ln -fs $SCRIPT_DIR/com.local.KeyRemapping.plist ~/Library/LaunchAgents/com.local
 ln -fs $SCRIPT_DIR/.zshrc ~/.zshrc
 ln -fs $SCRIPT_DIR/.p10k.zsh ~/.p10k.zsh
 ln -fs $SCRIPT_DIR/.vimrc ~/.vimrc
+ln -fs $SCRIPT_DIR/.gitconfig ~/.gitconfig
 mkdir -p ~/.vim
 ln -fs $SCRIPT_DIR/vim-colors ~/.vim/colors
 
 # Configure powerlevel10k
 echo "Running p10k configuration..."
-p10k configure
+zsh -i -c "p10k configure"
